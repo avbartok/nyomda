b9e2f6a39f64a1991905a335f5c9c77e94ccb8ce
Glitch (nyomda) <none>
2023-11-22 14:25:10 +0000

    Import Website

--- ReadMe.md
+++
@@ -1,1 +0,0 @@
-nyomda riso collective
---
+++ scripts/overlay.js
@@ -0,0 +1,7 @@
+function openOverlay(container) {
+        container.getElementsByClassName("overlay")[0].style.display = "flex";
+    }
+
+    function closeOverlay(container) {
+        container.getElementsByClassName("overlay")[0].style.display = "none";
+    }
