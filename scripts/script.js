b351cc5c9d2edcbbda5d377e91e17e9b203acf61
avbartok <145062521+avbartok@users.noreply.github.com>
2024-01-07 19:40:18 +0100

    Main change to Crimson/Black
    Color changes, Fotoupload, Text review

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
     for (let i = 0; i < 1; i++) {
-        let left = Math.random() * ($(window).width() - 100);
-        let top = Math.random() * ($(window).height() - 100);
-        $('body').append(`<div id='star${i}' class='star' contentEditable='false' 
-                           style='left:${left}px; top:${top}px;'><b>PRINTS GO BY<br> WORKSHOP</b> <br> 14.-15.Juni 16 Uhr<br> Anmeldung</div>`);
+        let diameter = 150; // Set the diameter of the circle
+        let left = Math.random() * ($(window).width() - diameter);
+        let top = Math.random() * ($(window).height() - diameter);
+        $('body').append(`
+            <div id='star${i}' class='sticker' style='left:${left}px; top:${top}px; width:${diameter}px; height:${diameter}px;'>
+                <div class='sticker-content'>
+                    <b>OFFENE WERKSTATT</b> <br> Dienstag 19.12. ab 17-21h
+                </div>
+            </div>
+        `);
     }
 
-      for (let i = 0; i < 1; i++) {
-        let left = Math.random() * ($(window).width() - 100);
-        let top = Math.random() * ($(window).height() - 100);
-        $('body').append(`<div id='star${i}' class='star' contentEditable='false' 
-                           style='left:${left}px; top:${top}px;'><b>NEW PRINTS<br> IDA SCHIELE - BODYFORMS </b> <br> A5, 14 Seiten, 2c<br> Auflage 25 Stück</div>`);
-    }
-  
-    $('.star').draggable();
+    $('.sticker').draggable();
 });
